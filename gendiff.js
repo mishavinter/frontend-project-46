f492a8d7e09bdd5b44565f6cb6e9fc1740e0164c
Michael Winter <mishavinter@mail.ru>
2025-04-25 22:51:46 +0500

    changed arguments topology in gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -7,6 +7,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version')
   .option('-f, --format [type]', 'output format')
-  .arguments('<filepath1> <filepath2>');
+  .argument('<filepath1>', 'path to the first file')
+  .argument('<filepath2>', 'path to the second file');
 
 program.parse();
