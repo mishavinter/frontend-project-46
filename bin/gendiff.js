8fcaa6cdface68f7ba0294bac832265a164d01ed
Michael Winter <mishavinter@mail.ru>
2025-04-26 01:16:33 +0500

    added imports and action to index.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,21 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import path from 'path';
+import fs from 'fs';
+import gendiff from '../src/index.js';
+
 const program = new Command();
 
 program
+  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version')
   .option('-f, --format [type]', 'output format')
   .argument('<filepath1>', 'path to the first file')
-  .argument('<filepath2>', 'path to the second file');
+  .argument('<filepath2>', 'path to the second file')
+  .action((a, b, options) => {
+    console.log(gendiff(a, b, options));
+  });
 
 program.parse();
