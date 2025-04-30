f590cc07b2bb5f40620986b4238fcc96ecbcd51d
Michael Winter <mishavinter@mail.ru>
2025-05-01 00:00:27 +0500

    updated index.test.js by adding yaml-test

--- __tests__/src/index.test.js
+++ __tests__/src/index.test.js
@@ -8,12 +8,18 @@ import gendiff from '../../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// BEGIN (write your solution here)
 const json1 = `${__dirname}/../__fixtures__/file1.json`;
 const json2 = `${__dirname}/../__fixtures__/file2.json`;
+const resultJson = fs.readFileSync(`${__dirname}/../__fixtures__/result.json`, 'utf-8');
 
-const result = fs.readFileSync(`${__dirname}/../__fixtures__/result.json`, 'utf-8');
+test('check json-files equality', () => {
+  expect(gendiff(json1, json2)).toEqual(resultJson);
+});
 
-test('check files equality', () => {
-  expect(gendiff(json1, json2)).toEqual(result);
+const yaml1 = `${__dirname}/../__fixtures__/yaml1.json`;
+const yaml2 = `${__dirname}/../__fixtures__/yaml2.json`;
+const resultYaml = fs.readFileSync(`${__dirname}/../__fixtures__/result.yaml`, 'utf-8');
+
+test('check yaml-files equality', () => {
+  expect(gendiff(yaml1, yaml2)).toEqual(resultYaml);
 });
