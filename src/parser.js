e47bd28d31c61d97d7afa7959bf755e6eda3ba5e
Michael Winter <mishavinter@mail.ru>
2025-05-01 00:09:27 +0500

    updated parser.js by adding possibility to parse yaml

--- src/parser.js
+++ src/parser.js
@@ -1,1 +1,17 @@
-export default (file) => JSON.parse(file);
+import fs from 'fs';
+import yaml from 'js-yaml';
+
+
+export default (file) => {
+    const configPath = 'path/to/eslint';
+    const format = path.extname(configPath);
+    const data = fs.readSync(configPath);
+
+    let parse;
+    if (format === '') {
+      parse = JSON.parse;
+    } else if (format === '.yml') {
+      parse = yaml.safeLoad;
+    }
+    return parse(file);
+};
