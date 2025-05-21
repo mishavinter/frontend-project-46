dd7df228099b8c9c2b4fb5a684e984aeefe65f15
Michael Winter <mishavinter@mail.ru>
2025-05-21 23:10:17 +0500

    updated src/index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,17 @@
 import path from 'path';
 import fs from 'fs';
-import parser from './parser.js';
-import _ from 'lodash';
+import parse from './parser.js';
 
-export default (filepath1, filepath2) => {
+import makeInternalRepresentation from './makeInternalRepresentation.js';
+import formatDiff from '../formatters/index.js';
+
+export default (filepath1, filepath2, format = 'stylish') => {
   const obj1 = fs.readFileSync(path.resolve(filepath1), 'utf-8');
   const obj2 = fs.readFileSync(path.resolve(filepath2), 'utf-8');
 
-  const parsedObj1 = parser(obj1);
-  const parsedObj2 = parser(obj2);
+  const parsedObj1 = parse(obj1);
+  const parsedObj2 = parse(obj2);
 
-  const keys1 = Object.keys(parsedObj1);
-  const keys2 = Object.keys(parsedObj2);
-  const keys = _.uniq([...keys1, ...keys2]);
-
-  let result = '';
-  for (const key of keys) {
-    if (!Object.hasOwn(parsedObj1, key)) {
-      result += `+ ${key}: ${parsedObj2[key]}\n`;
-    } else if (!Object.hasOwn(parsedObj2, key)) {
-      result += `- ${key}: ${parsedObj1[key]}\n`;
-    } else if (parsedObj1[key] !== parsedObj2[key]) {
-      result += `- ${key}: ${parsedObj1[key]}\n+ ${key}: ${parsedObj2[key]}\n`;
-    } else {
-      result += `  ${key}: ${parsedObj1[key]}\n`
-    }
-  };
-
-  return result;
+  const internalRepresentation = makeInternalRepresentation(parsedObj1, parsedObj2);
+  return formatDiff(internalRepresentation, format);
 };
