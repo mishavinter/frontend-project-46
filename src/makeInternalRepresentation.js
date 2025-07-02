3f614f7cf5cebb2ba1f7276aa72cee5151ed90d2
Michael Winter <mishavinter@mail.ru>
2025-07-02 23:25:19 +0500

    update logics in makeInternalRepresentation function

--- src/makeInternalRepresentation.js
+++ src/makeInternalRepresentation.js
@@ -1,32 +1,28 @@
 import _ from 'lodash'
 
 const makeInternalRepresentation = (obj1, obj2) => {
-  if (!obj1 || !obj2) {
-    throw new Error('One or both objects are undefined or null')
-  }
-
   const keys1 = Object.keys(obj1)
   const keys2 = Object.keys(obj2)
-  const keys = _.uniq([...keys1, ...keys2])
+  const keys = _.union(keys1, keys2).sort()
 
   let result = {}
   for (const key of keys) {
-    if (!Object.hasOwn(obj1, key)) {
-      result[key] = { type: 'added', value: obj2[key] }
-    }
-    else if (!Object.hasOwn(obj2, key)) {
-      result[key] = { type: 'deleted', value: obj1[key] }
-    }
-    else if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
       result[key] = {
         type: 'nested',
         children: makeInternalRepresentation(obj1[key], obj2[key]),
       }
     }
+    else if (!Object.hasOwn(obj1, key)) {
+      result[key] = { type: 'added', value: obj2[key] }
+    }
+    else if (!Object.hasOwn(obj2, key)) {
+      result[key] = { type: 'deleted', value: obj1[key] }
+    }
     else if (obj1[key] !== obj2[key]) {
       result[key] = { type: 'changed', oldValue: obj1[key], newValue: obj2[key] }
     }
-    else {
+    else if (obj1[key] === obj2[key]) {
       result[key] = { type: 'unchanged', value: obj1[key] }
     }
   }
