2ddc05318e50b7e4de11f948414b5676a225cba4
Michael Winter <mishavinter@mail.ru>
2025-07-03 00:15:28 +0500

    added file with object sorting function

---
+++ src/sortKeys.js
@@ -0,0 +1,12 @@
+const sortKeys = (obj) => {
+  if (typeof obj !== 'object' || obj === null) {
+    return obj
+  }
+
+  const sortedEntries = Object.entries(obj)
+    .sort(([keyA], [keyB]) => keyA.localeCompare(keyB))
+    .map(([key, value]) => [key, sortKeys(value)])
+  return Object.fromEntries(sortedEntries)
+}
+ export default sortKeys
+ 
