8f4a119aa173a4c604133d1660a64539abbc8df1
Michael Winter <mishavinter@mail.ru>
2025-07-03 22:44:12 +0500

    added test logics for json formatted operations check

--- __tests__/src/index.test.js
+++ __tests__/src/index.test.js
@@ -8,21 +8,21 @@ const __dirname = path.resolve()
 
 const json1 = `${__dirname}/__fixtures__/file1.json`
 const json2 = `${__dirname}/__fixtures__/file2.json`
-const result = fs.readFileSync(`${__dirname}/__fixtures__/result.txt`, 'utf-8')
+const resultStylish = fs.readFileSync(`${__dirname}/__fixtures__/resultStylish.txt`, 'utf-8')
 
 test('check equality of JSON files with stylish format', () => {
-  expect(gendiff(json1, json2)).toEqual(result)
+  expect(gendiff(json1, json2)).toEqual(resultStylish)
 })
 
 const yaml1 = `${__dirname}/__fixtures__/file1.yaml`
 const yaml2 = `${__dirname}/__fixtures__/file2.yaml`
 
 test('check equality of YAML files with stylish format', () => {
-  expect(gendiff(yaml1, yaml2)).toEqual(result)
+  expect(gendiff(yaml1, yaml2)).toEqual(resultStylish)
 })
 
 test('check equality of JSON and YAML files with stylish format', () => {
-  expect(gendiff(json1, yaml2)).toEqual(result)
+  expect(gendiff(json1, yaml2)).toEqual(resultStylish)
 })
 
 const resultPlain = fs.readFileSync(`${__dirname}/__fixtures__/resultPlain.txt`, 'utf-8')
@@ -37,4 +37,18 @@ test('check equality of YAML files with plain format', () => {
 
 test('check equality of JSON and YAML files with plain format', () => {
   expect(gendiff(json1, yaml2, 'plain')).toEqual(resultPlain)
+})
+
+const resultJson = fs.readFileSync(`${__dirname}/__fixtures__/resultJson.txt`, 'utf-8')
+
+test('check equality of JSON files with json format', () => {
+  expect(gendiff(json1, json2, 'json')).toEqual(resultJson)
+})
+
+test('check equality of YAML files with json format', () => {
+  expect(gendiff(yaml1, yaml2, 'json')).toEqual(resultJson)
+})
+
+test('check equality of JSON and YAML files with json format', () => {
+  expect(gendiff(json1, yaml2, 'json')).toEqual(resultJson)
 })
