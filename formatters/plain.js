4eba0acb76cc07c2e804f9be61a8ba6cb8794c82
Michael Winter <mishavinter@mail.ru>
2025-07-03 22:00:29 +0500

    updated logics in plain formatter

--- formatters/plain.js
+++ formatters/plain.js
@@ -1,19 +1,17 @@
 const plainFormatter = (diff) => {
   const iter = (currentDiff, path) => {
-    return currentDiff.flatMap(({ key, type, value }) => {
+    return Object.entries(currentDiff).flatMap(([key, { type, value, oldValue, newValue, children }]) => {
       const propertyPath = path ? `${path}.${key}` : key
 
       switch (type) {
+        case 'nested':
+          return iter(children, propertyPath)
         case 'added':
           return `Property '${propertyPath}' was added with value: ${formatValue(value)}`
         case 'deleted':
           return `Property '${propertyPath}' was removed`
-        case 'changed': {
-          const { oldValue, newValue } = value
+        case 'changed':
           return `Property '${propertyPath}' was updated. From ${formatValue(oldValue)} to ${formatValue(newValue)}`
-        }
-        case 'nested':
-          return iter(value.children, propertyPath) // Рекурсивно обрабатываем вложенные изменения
         default:
           return []
       }
@@ -24,11 +22,11 @@ const plainFormatter = (diff) => {
 }
 
 // Преобразователь значений для вывода
-const formatValue = (val) => {
-  if (typeof val === 'object' && val !== null) {
+const formatValue = (value) => {
+  if (typeof value === 'object' && value !== null) {
     return '[complex value]'
   }
-  return typeof val === 'string' ? `'${val}'` : val
+  return typeof value === 'string' ? `'${value}'` : value
 }
 
 export default plainFormatter
