5620824ac5fe24c54b8f23c8b41a93d51cbd13f4
Michael Winter <mishavinter@mail.ru>
2025-07-03 21:10:54 +0500

    modified test logics for stylish formatted operations check

--- __tests__/src/index.test.js
+++ __tests__/src/index.test.js
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 
-import { fileURLToPath } from 'url'
 import fs from 'fs'
 import path from 'path'
 import gendiff from '../../src/index.js'
@@ -9,22 +8,19 @@ const __dirname = path.resolve()
 
 const json1 = `${__dirname}/__fixtures__/file1.json`
 const json2 = `${__dirname}/__fixtures__/file2.json`
-const resultJson = fs.readFileSync(`${__dirname}/__fixtures__/result.json`, 'utf-8')
+const result = fs.readFileSync(`${__dirname}/__fixtures__/result.txt`, 'utf-8')
 
 test('check equality of JSON files with stylish format', () => {
-  expect(gendiff(json1, json2)).toEqual(resultJson)
+  expect(gendiff(json1, json2)).toEqual(result)
 })
 
 const yaml1 = `${__dirname}/__fixtures__/file1.yaml`
 const yaml2 = `${__dirname}/__fixtures__/file2.yaml`
-const resultYaml = fs.readFileSync(`${__dirname}/__fixtures__/result.yaml`, 'utf-8')
 
 test('check equality of YAML files with stylish format', () => {
-  expect(gendiff(yaml1, yaml2)).toEqual(resultYaml)
+  expect(gendiff(yaml1, yaml2)).toEqual(result)
 })
 
-const resultPlain = fs.readFileSync(`${__dirname}/__fixtures__/resultPlain.txt`, 'utf-8')
-
-test('check equality of JSON files with plain format', () => {
-  expect(gendiff(json1, json2, 'plain')).toEqual(resultPlain)
+test('check equality of JSON and YAML files with stylish format', () => {
+  expect(gendiff(json1, yaml2)).toEqual(result)
 })
